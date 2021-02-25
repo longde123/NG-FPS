34eb76bf0508873134cc65f5aa6db1876c6e2209
Josip Petanjek <jpetanjek@foi.hr>
2021-02-25 18:04:44 +0100

    Initial Takeover prototype / getting all players, trying to spawn a character for them from a spawn point

---
+++ Source/GDKShooter/Private/Player_AI_Tracker.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) Improbable Worlds Ltd, All Rights Reserved
+
+
+#include "Player_AI_Tracker.h"
+#include "GameFramework/GameStateBase.h"
+
+Player_AI_Tracker::Player_AI_Tracker()
+{
+}
+
+Player_AI_Tracker::~Player_AI_Tracker()
+{
+}
